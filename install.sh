9f37fcc870c4810f87798fbebbf32d9d6950c3b2
Neil Castelino <neil.fl93@gmail.com>
2016-02-06 00:16:45 -0500

    added initial install script. Doesn't do anything but find system distro info

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#------------------------------------------------------------------------------
+# Add stuff here later
+#------------------------------------------------------------------------------
+
+# Variables we would find important
+PACMAN=""		# Package manager
+UPDATE="update"		# Package manager option to update system
+UPGRADE="upgrade"	# Package manager option to upgrade system
+INSTALL_PATH=""		# Installation path
+
+# Dependencies
+#TODO Add real dependencies..
+DEPENDENCIES="git eclipse"
+
+# Find out what system we're working with
+pretty_name=$(cat /etc/os-release | grep PRETTY_NAME | sed -e 's/.*="\(.*\)"/\1/')
+release_id=$(cat /etc/os-release | grep ^ID | sed -e 's/.*=\(.*\)/\1/')
+echo "System details:"
+echo -e "\tOS: $pretty_name"
+
+# Init package manager details
+if [ "x$release_id" == "xdebian" ]; then
+	PACMAN="apt"
+elif [ "x$relese_id" == "xfedora" ]; then
+	PACMAN="yum"
+fi
+echo -e "\tUsing package manager: $PACMAN"
+
+#TODO Add the real stuff
