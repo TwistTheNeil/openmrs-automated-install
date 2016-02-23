af0b345e66adc152f6fd5b775de9f0e608c2fac2
Nathan Bender <neb45@drexel.edu>
2016-02-23 14:28:12 -0500

    Added notes for mac. Also added some comments for mac script

--- mac-install.sh
+++ mac-install.sh
@@ -15,8 +15,8 @@ if [ "$(uname)" == "Darwin" ]; then
     
 	#Install brew-cask in order to install java
 	brew tap caskroom/cask
-	brew install brew-cask
-	brew cask install java
+	brew install brew-cask # install brew-cask since java is not directly supported by brew
+	brew cask install java # install java
 
 	# Install dependencies
         for dep in "$DEPENDENCIES"; do
@@ -27,7 +27,7 @@ if [ "$(uname)" == "Darwin" ]; then
 	/usr/local/Cellar/tomcat/*/libexec/bin/shutdown.sh &
 
 	# Notify user about the need for a password change
-        less notes/tomcat-user
+        less notes/tomcat-user-mac
 	
 	# Copy template tomcat users file to /etc/tomcatX/ and fix permissions
         cp templates/tomcat-users.xml /usr/local/Cellar/tomcat/*/libexec/conf/
@@ -36,10 +36,15 @@ if [ "$(uname)" == "Darwin" ]; then
 	# Start tomcat
 	/usr/local/Cellar/tomcat/*/libexec/bin/startup.sh &
 
+	# Notify the user about deploying OpenMRS
+	less notes/deploy-mac
+
 	# Download openmrs.war (This scrapes the openmrs website and this command may
         # break at any point in time. Sorry about that..)
         # I specify it to download to /User/Downloads
         wget $(curl -s http://openmrs.org/download/ | grep sourceforge | grep openmrs.war | head -n 1 | awk -F'"' '{print $2}' | sed -e 's/\/download$//') -P ~/Downloads/
+
+	scriptloc=$(pwd)
 
         # Attempt to deploy openmrs using tomcat
         cd /usr/local/Cellar/tomcat/*/libexec/webapps/
@@ -51,9 +56,14 @@ if [ "$(uname)" == "Darwin" ]; then
         # Wait a few seconds for tomcat to discover it
         sleep 3
 
+	cd $scriptloc
+
+	# Show closing note
+	less notes/closing-mac
+
 	# Open OpenMRS in Firefox
 	/Applications/Firefox.app/Contents/MacOS/firefox http://localhost:8080/openmrs &
-elif
+else
 	echo "OS: $(uname)"
 	echo "Make sure you're running the correct script!"
 	exit 2
